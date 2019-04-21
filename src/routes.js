6da9f2dae676c60fa7b5e2933523aa5e1a0d2d25
Willian Home <willian@wiki4fit.com.br>
2019-04-21 18:47:44 -0300

    diretorios organizados

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = express.Router();
 const handle = require("express-async-handler");
 
-const UserController = require("./controllers/UserController");
+const { UserController } = require("./app/controllers");
 
 routes.post("/usuarios", handle(UserController.store));
 
